8073449e96a858216402ff8f487953e1be6433f8
Julia Cassidy <jac1007@wildcats.unh.edu>
2019-07-26 20:06:33 -0400

    creating trivia questions

---
+++ java/questions.js
@@ -0,0 +1,37 @@
+const quizQuestions = [
+    {
+        question: "This franchise played in Cleveland, Los Angeles, and St. Louis. Which team is this?",
+        choices: ["Rams", "Chargers", "Browns", "Vikings"],
+        correctAnswer: "Rams"
+    },
+    {
+        question: "The 2008 Indianapolis Colts franchise joined the NFL in 1953 under which name?",
+        choices: ["Indianapolis Colts", "Cleveland Colts", "Baltimore Colts", "New York Colts"],
+        correctAnswer: "Baltimore Colts"
+    },
+    {
+        question: "In what city did the Washington Redskins originally play?",
+        choices: ["New York", "Los Angeles", "Tampa", "Boston"],
+        correctAnswer: "Boston"
+    },
+    {
+        question:
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
