686eeaacf54b50f3ab49161ba104b22186da6fcf
Julia Cassidy <jac1007@wildcats.unh.edu>
2019-07-26 20:20:16 -0400

    adding more questions for trivia

--- java/questions.js
+++ java/questions.js
@@ -1,22 +1,48 @@
 const quizQuestions = [
-    {
+    { //1
         question: "This franchise played in Cleveland, Los Angeles, and St. Louis. Which team is this?",
         choices: ["Rams", "Chargers", "Browns", "Vikings"],
         correctAnswer: "Rams"
     },
-    {
+    { //2
         question: "The 2008 Indianapolis Colts franchise joined the NFL in 1953 under which name?",
         choices: ["Indianapolis Colts", "Cleveland Colts", "Baltimore Colts", "New York Colts"],
         correctAnswer: "Baltimore Colts"
     },
-    {
+    { //3
         question: "In what city did the Washington Redskins originally play?",
         choices: ["New York", "Los Angeles", "Tampa", "Boston"],
         correctAnswer: "Boston"
     },
-    {
+    { //4
+        question: "Which team was given the nickname The Ain'ts during the 1980 NFL season?",
+        choices: ["New Orleans Saints", "Cincinnati Bengals", "Green Bay Packers", "Chicago Bears"],
+        correctAnswer: "New Orleans Saints "
+    },
+    { //5
+        question: "The Miami Dolphins had the first undefeated season in NFL history. In 1985, which undefeated team did they beat, to make sure their legacy remained?",
+        choices: ["Buffalo Bills", "Chicago Bears", "Tennessee Titans", "Oakland Raiders"],
+        correctAnswer: "Chicago Bears"
+    },
+    { //6
+        questions: "Which team was given the nickname America's Team in the 1979 NFL season?",
+        choices: ['Houston Texans', "New York Giants", "New England Patriots", "Dallas Cowboys"],
+        correctAnswer: "Dallas Cowboys"
+    },
+    { //7
+        question: "Which team has lost five Super Bowls?",
+        choices: ["Minnesota Vikings", "New England Patriots", "Buffalo Bills", "Kansas City Chiefs"],
+        correctAnswer: "New England Patriots"
+    },
+    { //8
+        question: "Which team was wide reciever Antonio Brown recently traded to, after spending 8 seasons with the Pittsburgh Steelers?",
+        choices: ["Oakland Raiders", "Buffalo Bills", "New York Jets", "Los Angeles Chargers"],
+        correctAnswer: "Oakland Raiders"
+    },
+    { //9
         question:
-
+        choices:
+        correctAnswer:
     }
 
 
